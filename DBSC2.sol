b509f680b652e9caae6b6e78cd2111d1d7b757c3
Engine Eternal <51864059+MaxBotCoder@users.noreply.github.com>
2025-05-19 21:58:35 -0700

    Update and rename DBSC.sol to DBSC2.sol

---
+++ DBSC2.sol
@@ -0,0 +1,49 @@
+//SPDX-License-Identifier: MIT
+pragma solidity 0.8.0;
+
+contract TokenDeposit {
+
+    address LackOfAddress;
+    mapping(address => uint) Interactions;
+
+    struct SpecificTransaction {
+        uint _ValueStoredAtTheTime;
+        uint _SpecificDepositValue;
+        uint _SpecificWithDrawlValue;
+        address _RecipientValue;
+        uint _SpecificTimeStamp;
+    }
+
+    mapping (address => mapping(uint => SpecificTransaction)) public specifictransaction;
+
+    struct Transactions{
+        uint _ValueStored;
+        uint _LatestDepositValue;
+        uint _WithdrawlValue;
+        uint _WithdrawlRecipient;
+        uint _TransactionTime;
+        uint _TransactionNumber;
+    }
+
+    mapping (address => Transactions) public transaction;
+
+    function DepositMoney () public payable {
+        transaction[msg.sender]._LatestDepositValue = msg.value;
+        transaction[msg.sender]._ValueStored += msg.value;
+        transaction[msg.sender]._TransactionTime = block.timestamp;
+        Interactions[msg.sender]++;
+        transaction[msg.sender]._TransactionNumber = Interactions[msg.sender];
+        specifictransaction[msg.sender][Interactions[msg.sender]] = SpecificTransaction(transaction[msg.sender]._ValueStored, transaction[msg.sender]._LatestDepositValue, 0, LackOfAddress, transaction[msg.sender]._TransactionTime);
+    }
+    
+    function WithdrawMoney (address ToWhom, uint AmountToWithdraw) public {
+        if(AmountToWithdraw <= transaction[msg.sender]._ValueStored){
+        transaction[msg.sender]._ValueStored -= AmountToWithdraw;   
+        transaction[msg.sender]._WithdrawlValue = AmountToWithdraw;
+        transaction[msg.sender]._TransactionTime = block.timestamp;
+        Interactions[msg.sender]++;
+        transaction[msg.sender]._TransactionNumber = Interactions[msg.sender];
+        specifictransaction[msg.sender][Interactions[msg.sender]] = SpecificTransaction(transaction[msg.sender]._ValueStored, 0, transaction[msg.sender]._WithdrawlValue = AmountToWithdraw, ToWhom, transaction[msg.sender]._TransactionTime);
+        }
+    }
+}
